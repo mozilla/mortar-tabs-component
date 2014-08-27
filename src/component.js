ed03c57c05bb70090f0f352a78fa08713179eb7d
sole <sole@mozilla.com>
2014-08-27 22:17:47 +0100

    starting this again

---
+++ src/component.js
@@ -0,0 +1,103 @@
+(function() {
+
+  console.log(this);
+  
+  var proto = Object.create(HTMLElement.prototype);
+
+  proto.createdCallback = function() {
+    this.attachedTo = undefined;
+    this.addEventListener('click', this.onClick);
+
+  };
+
+  function deselectNode(el) {
+    el.removeAttribute('selected');
+  }
+
+  function selectNode(el) {
+    el.setAttribute('selected', 'selected');
+  }
+
+  // methods
+  proto.show = function(index) {
+    console.log('show me', index);
+
+    var children = this.getChildrenNodes();
+    children.forEach(deselectNode);
+    selectNode(children[index]);
+
+    if(this.attachedTo) {
+      this.attachedTo.show(index);
+    }
+
+  };
+
+  // This method links these tabs to another element that has multiple children and a 'show' method - so when clicking on the tabs we'll call the show method on that element we're linked to
+  proto.attachTo = function(el) {
+    /*if(el.show !== undefined && typeof el.show === 'function') {
+      this.attachedTo = el;
+    }*/
+  };
+
+  //
+
+  proto.getChildrenNodes = function() {
+    var children = this.childNodes;
+    var actualChildren = [];
+
+    for(var i = 0; i < children.length; i++) {
+      var child = children[i];
+
+      // Discard whitespace sort of nodes
+      if(child.tagName === undefined) {
+        continue;
+      }
+
+      actualChildren.push(child);
+
+    }
+
+    return actualChildren;
+
+  };
+
+  proto.onClick = function(ev) {
+
+    var target = ev.target;
+
+    var children = this.getChildrenNodes();
+    var child;
+
+    for(var i = 0; i < children.length; i++) {
+
+      child = children[i];
+
+      if(child == target) {
+        this.show(i);
+        return;
+      }
+
+    }
+
+  };
+
+
+  var component = {};
+
+  component.prototype = proto;
+
+  component.register = function(name) {
+    document.registerElement(name, {
+      prototype: proto
+    });
+  };
+
+  if(typeof define === 'function' && define.amd) {
+    define(function() { return component; });
+  } else if(typeof module !== 'undefined' && module.exports) {
+    module.exports = component;
+  } else {
+    component.register('mortar-tabs'); // automatic registration
+  }
+
+}).call(this);
